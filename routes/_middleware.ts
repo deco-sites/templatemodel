f195821b5c973daac604d0e44b2c2c928fb7b633
Matheus Gaudencio <matheusgr@gmail.com>
2023-01-26 15:14:55 -0300

    Fixed domains error list format.

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -3,5 +3,5 @@ import { withLive } from "$live/live.ts";
 export const handler = withLive({
   siteId: 482,
   site: "templatemodel",
-  domains: ["deco-sites-templatemodel.deno.dev, your.deco.site"],
+  domains: ["deco-sites-templatemodel.deno.dev", "your.deco.site"],
 });
