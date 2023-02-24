70fef2044398f9456f311e485ae897b1438ffa57
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2023-02-24 13:12:02 -0300

    Update sections/TemplateSpacer.tsx
    Co-authored-by: Igor Brasileiro <brasileiro456@gmail.com>

--- sections/TemplateSpacer.tsx
+++ sections/TemplateSpacer.tsx
@@ -1,5 +1,2 @@
-import Spacer, { Props } from "$store/sections/Spacer.tsx";
-
-export default function (props: Props) {
-  return <Spacer {...props}></Spacer>;
-}
+export { default } from "$store/sections/Spacer.tsx";
+export type { Props } from "$store/sections/Spacer.tsx";
